c70da9bd9e44a1e0cbb543ddab5ea6ab99510dd6
EduardoNFraiz <eduardonfraiz@gmail.com>
2025-09-11 08:58:42 -0300

    pag 13 passo 20

---
+++ app/src/main/java/com/eduardo/navigationdrawer/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.eduardo.navigationdrawer
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.fragment.app.FragmentManager
+import com.eduardo.navigationdrawer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var fragmentManager: FragmentManager
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        WindowCompat.setDecorFitsSystemWindows(window,false)
+        WindowInsetsControllerCompat(window, window.decorView).hide(WindowInsetsCompat.Type.statusBars())
+
+        setSupportActionBar(binding.toolbar)
+
+        val toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar, R.string.nav_open, R.string.nav_close)
+        toggle.syncState()
+
+        //supportActionBar?.title =""
+        binding.navigationDrawer.setNavigationItemSelectedListener(this)
+    }
+}
