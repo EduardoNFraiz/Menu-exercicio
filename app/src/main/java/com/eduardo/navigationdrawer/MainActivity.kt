a48e99de27ea82265b3931ddb30c3e2c494aa630
EduardoNFraiz <eduardonfraiz@gmail.com>
2025-09-12 08:56:44 -0300

    padrao pronto

--- app/src/main/java/com/eduardo/navigationdrawer/MainActivity.kt
+++ app/src/main/java/com/eduardo/navigationdrawer/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.eduardo.navigationdrawer
 
+import android.app.Fragment
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.addCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.fragment.app.FragmentManager
 import com.eduardo.navigationdrawer.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var fragmentManager: FragmentManager
     private lateinit var binding: ActivityMainBinding
@@ -31,5 +37,48 @@ class MainActivity : AppCompatActivity() {
 
         //supportActionBar?.title =""
         binding.navigationDrawer.setNavigationItemSelectedListener(this)
+
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.bottom_home -> openFragment(HomeFragment())
+                R.id.bottom_cart -> openFragment(CartFragment())
+                R.id.bottom_profile -> openFragment(ProfileFragment())
+                R.id.bottom_menu -> openFragment(MenuFragment())
+            }
+            true
+        }
+        fragmentManager = supportFragmentManager
+        openFragment(HomeFragment())
+
+        binding.fab.setOnClickListener {
+            Toast.makeText(this, "Categorias", Toast.LENGTH_SHORT).show()
+        }
+
+        onBackPressedDispatcher.addCallback(this){
+            if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+                binding.drawerLayout.closeDrawer(GravityCompat.START)
+            }else{
+                finish()
+            }
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_prime -> openFragment(PrimeFragment())
+            R.id.nav_fashion -> openFragment(FashionFragment())
+            R.id.nav_eletronics -> openFragment(ElectronicsFragment())
+            R.id.nav_Beauty -> Toast.makeText(this,"Beleza", Toast.LENGTH_SHORT).show()
+            R.id.nav_Food -> Toast.makeText(this,"Alimentos", Toast.LENGTH_SHORT).show()
+            R.id.nav_furniture -> Toast.makeText(this,"Móveis", Toast.LENGTH_SHORT).show()
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun openFragment(fragment: androidx.fragment.app.Fragment) {
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container,fragment)
+        fragmentTransaction.commit()
     }
 }
